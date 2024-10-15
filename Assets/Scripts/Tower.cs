fd58ba0ea59f786bef4db0639260456408ac670f
jejisova <100522165+jejisova@users.noreply.github.com>
2024-10-15 23:08:06 +0300

    Towers can choose an ememy
    Created script for towers. Now they can choose closest ememy and look at it.
    
    and fix very terrible bag with towers rotation.

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,52 +1,93 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+  using System;
+  using System.Collections;
+  using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Animations;
+  using UnityEngine.Animations;
 
 
 
-public class Tower : MonoBehaviour
-{   
-    [SerializeField] Transform towerTop;
-    [SerializeField] Transform targetEnemy;
-    [SerializeField] float shootRange;
-    [SerializeField] ParticleSystem bulletParticle;
- 
-    void Update()
-    { 
-
-      
-      towerTop.LookAt(targetEnemy);
-      if (targetEnemy)
-      {
-        Fire();
-      }
-      else
-      {
-        Shoot(false);
-      }
-      
-     
-
+  public class Tower : MonoBehaviour
+  {   
+      [SerializeField] Transform towerTop;
+      [SerializeField] Transform targetEnemy;
+      [SerializeField] float shootRange;
+      [SerializeField] ParticleSystem bulletParticle;
+  
+      void Update()
+      { 
+        SetTargetEnemy();
         
-    }
 
-    private void Fire()
-    {
-        float distanceToEnemy = Vector3.Distance(targetEnemy.position, transform.position);
-        if (distanceToEnemy < shootRange)
-        {
-          Shoot(true);
+        if (targetEnemy)
+        { towerTop.LookAt(new Vector3(targetEnemy.position.x, towerTop.transform.position.y, targetEnemy.position.z));
+
+          Fire();
         }
-        else{
+        else
+        {
           Shoot(false);
         }
-    }
+        
+      
 
-    private void Shoot(bool isActive)
-    {
-      var emission = bulletParticle.emission;   
-      emission.enabled = isActive;
-    }
-}
+          
+      }
+
+      private void SetTargetEnemy()
+      {
+
+        var sceneEnemies = FindObjectsOfType<EnemyDamage>();
+  
+        if (sceneEnemies.Length == 0)
+        {return;}
+        
+        Transform closestEnemy = sceneEnemies[0].transform;
+
+        foreach(EnemyDamage test in sceneEnemies)
+        { 
+          closestEnemy = GetClosestEnemy(closestEnemy.transform, test.transform);
+
+        }
+
+        targetEnemy = closestEnemy;
+
+      }
+
+      private Transform GetClosestEnemy(Transform EnemyA, Transform EnemyB)
+      { 
+        var distToA = Vector3.Distance(EnemyA.position, transform.position);
+         var distToB = Vector3.Distance(EnemyB.position, transform.position);
+
+         if(distToA < distToB)
+         {
+          return EnemyA;
+         }
+         else
+         {
+          return EnemyB;
+         }
+        
+    
+
+      }
+
+      private void Fire()
+      {
+          float distanceToEnemy = Vector3.Distance(targetEnemy.position, transform.position);
+          if (distanceToEnemy < shootRange)
+          {
+            Shoot(true);
+          }
+          else{
+            Shoot(false);
+          }
+      }
+
+      private void Shoot(bool isActive)
+      {
+        var emission = bulletParticle.emission;   
+        emission.enabled = isActive;
+      }
+  }
